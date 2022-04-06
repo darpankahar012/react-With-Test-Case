b6a7cffbd4b4f193a1ac3beb01c0e789c7a78304
darpan@webmynesystems.com <darpan@webmynesystems.com>
2022-04-06 18:44:55 +0530

    react with test case

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyInput from './MyInput';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <MyInput />
+  </div>
+);
+
+export default App;
